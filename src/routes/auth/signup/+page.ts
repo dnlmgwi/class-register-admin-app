e4c9ddce0b491a3bcb0e5cfacef9a078d8d1a904
Daniel P. Mgawi <pdmgawi@gmail.com>
2024-08-02 03:49:00 +0200

    Update +page.ts

--- src/routes/auth/signup/+page.ts
+++ src/routes/auth/signup/+page.ts
@@ -2,11 +2,15 @@ import { superValidate } from "sveltekit-superforms";
 import { zod } from "sveltekit-superforms/adapters";
 import type { PageLoad } from "./$types";
 import { createUserSchema } from "$lib/domain/validators/User/createUserValidator";
+import { createStudentSchema } from "$lib/domain/validators/User/createStudentValidator";
 
 export const load: PageLoad = async () => {
   const form = await superValidate(zod(createUserSchema));
 
+  const studentForm = await superValidate(zod(createStudentSchema));
+
   return {
     form,
+    studentForm
   };
 };
