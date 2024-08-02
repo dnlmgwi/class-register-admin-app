1553396e9cf2386d9df1fe3a92805e6faed3a17b
Daniel P. Mgawi <pdmgawi@gmail.com>
2024-08-02 03:48:25 +0200

    Update createStudentValidator.ts

--- src/lib/domain/validators/User/createStudentValidator.ts
+++ src/lib/domain/validators/User/createStudentValidator.ts
@@ -2,11 +2,14 @@ import { z } from "zod";
 
 export const createStudentSchema = z.object({
   phone: z.string().min(1, { message: "Phone is required" }),
-  password: z.string().min(5, { message: "5 Character Password is required" }),
-  country: z.string().min(2, { message: "Valid Country Code Required" }),
   firstName: z.string().min(1, { message: "First Name is required" }),
   lastName: z.string().min(1, { message: "Last Name is required" }),
   email: z.string().email({ message: "Invalid Email Address" }),
-  avatarUrl: z.string().url({ message: "Invalid Avatar URL" }),
+  avatarUrl: z
+    .string()
+    .url({ message: "Invalid Avatar URL" })
+    .default(
+      "https://csqaiejkcporxddqwans.supabase.co/storage/v1/object/public/uploads/218a145f-63ad-41ca-a32a-4ebceb6bdbd6/GHSTBUoW8AAoAHo.jpeg"
+    ),
   studentId: z.string().min(1, { message: "Student ID is required" }),
 });
